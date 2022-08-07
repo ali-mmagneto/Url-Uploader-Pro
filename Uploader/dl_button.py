b3cd13a6186990c874865a6e3b00a4813f76ea08
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-08-07 14:13:21 +0300

    Update dl_button.py

--- Uploader/dl_button.py
+++ Uploader/dl_button.py
@@ -13,6 +13,7 @@ import shutil
 import time
 from datetime import datetime
 from Uploader.config import Config
+from Uploader.config import Ubot
 from Uploader.script import Translation
 from Uploader.thumbnail import *
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
@@ -115,11 +116,83 @@ async def ddl_call_back(bot, update):
             # https://stackoverflow.com/a/678242/4723940
             file_size = os.stat(download_directory).st_size
         if file_size > Config.TG_MAX_FILE_SIZE:
-            await bot.edit_message_text(
-                chat_id=update.message.chat.id,
-                text=Translation.RCHD_TG_API_LIMIT,
-                message_id=update.message.id
-            )
+            if tg_send_type == "upload_as_file":
+                thumbnail = await Gthumb01(bot, update)
+                await Ubot.send_document(
+                    chat_id=Config.PRE_LOG,
+                    document=download_directory,
+                    thumb=thumbnail,
+                    caption=description,
+                    #parse_mode="HTML",
+                    reply_to_message_id=update.message.reply_to_message.id,
+                    progress=progress_for_pyrogram,
+                    progress_args=(
+                        Translation.UPLOAD_START,
+                        update.message,
+                        #custom_file_name,
+                        start_time
+                    )
+                )
+
+            elif tg_send_type == "video":
+                 width, height, duration = await Mdata01(download_directory)
+                 thumbnail = await Gthumb02(bot, update, duration, download_directory)
+                 await Ubot.send_video(
+                    chat_id=Config.PRE_LOG,
+                    video=download_directory,
+                    caption=description,
+                    # parse_mode="HTML",
+                    duration=duration,
+                    width=width,
+                    height=height,
+                    thumb=thumbnail,
+                    supports_streaming=True,
+                    reply_to_message_id=update.message.reply_to_message.id,
+                    progress=progress_for_pyrogram,
+                    progress_args=(
+                        Translation.UPLOAD_START,
+                        update.message,
+                        #custom_file_name,
+                        start_time
+                    )
+                )
+            if tg_send_type == "audio":
+                duration = await Mdata03(download_directory)
+                thumbnail = await Gthumb01(bot, update)
+                await Ubot.send_audio(
+                    chat_id=Config.PRE_LOG,
+                    audio=download_directory,
+                    caption=description,
+                   # parse_mode="HTML",
+                    duration=duration,
+                    thumb=thumbnail,
+                    reply_to_message_id=update.message.reply_to_message.id,
+                    progress=progress_for_pyrogram,
+                    progress_args=(
+                        Translation.UPLOAD_START,
+                        update.message,
+                        #custom_file_name,
+                        start_time
+                    )
+                ) 
+            elif tg_send_type == "vm":
+                width, duration = await Mdata02(download_directory)
+                thumbnail = await Gthumb02(bot, update, duration, download_directory)
+                await Ubot.send_video_note(
+                    chat_id=Config.PRE_LOG,
+                    video_note=download_directory,
+                    duration=duration,
+                    length=width,
+                    thumb=thumb_image_path,
+                    reply_to_message_id=update.message.reply_to_message.id,
+                    progress=progress_for_pyrogram,
+                    progress_args=(
+                        Translation.UPLOAD_START,
+                        update.message,
+                        #custom_file_name,
+                        start_time
+                    )
+                )    
         else:
             # ref: message from @SOURCES_CODES
             start_time = time.time()
