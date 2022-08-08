7f454230effeb566be28d79b6acc0f4df45e07c5
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-08-08 18:47:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,6 +22,16 @@ from Uploader.database.database import Database
 
 from pyrogram import Client
 
+try:
+    STRING_SESSION = getConfig('STRING_SESSION')
+    if len(STRING_SESSION) == 0:
+        raise KeyError
+    userbot = Client(name='userbot', api_id=Config.API_ID, api_hash=Config.API_HASH, session_string=Config.STRING_SESSION, parse_mode=enums.ParseMode.HTML, no_updates=True)
+    if not userbot:
+        LOGGER.error("Cannot initialized User Session. Please regenerate USER_SESSION_STRING")
+    else:
+        userbot.start() 
+
 if __name__ == "__main__" :
     # create download directory, if not exist
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
@@ -36,13 +46,3 @@ if __name__ == "__main__" :
     Warrior.db = Database(Config.DATABASE_URL, Config.BOT_USERNAME)
     Warrior.broadcast_ids = {}
     Warrior.run()
-
-    try:
-        STRING_SESSION = getConfig('STRING_SESSION')
-        if len(STRING_SESSION) == 0:
-            raise KeyError
-        userbot = Client(name='userbot', api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION, parse_mode=enums.ParseMode.HTML, no_updates=True)
-        if not userbot:
-            LOGGER.error("Cannot initialized User Session. Please regenerate USER_SESSION_STRING")
-        else:
-            userbot.start()
